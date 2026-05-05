0cfa3b8fdc8eb13a7d7774f498ef84dff69d4452
hemant <hy564636@gmail.com>
2026-05-05 18:42:01 +0530

    added transaction and withdrawl model

---
+++ Backend/src/modules/transaction/transaction.model.ts
@@ -0,0 +1,53 @@
+import mongoose, { Document, ObjectId, Schema } from "mongoose";
+
+export interface ITransaction extends Document{
+    userId:ObjectId,
+    amount:Number,
+    type:"credit"|"debit",
+    reason:"deposit"|"withdraw"|"tournament_join"|"tournament_refund"|"tournament_reward",
+    tournamentId?:ObjectId,
+    paymentId?:ObjectId,
+    withdrawalId?:ObjectId
+}
+
+const TransactionSchema = new Schema<ITransaction>({
+  userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+
+  amount: { type: Number, required: true },
+
+  type: {
+    type: String,
+    enum: ["credit", "debit"],
+    required: true
+  },
+
+  reason: {
+    type: String,
+    enum: [
+      "deposit",
+      "withdraw",
+      "tournament_join",
+      "tournament_refund",
+      "tournament_reward"
+    ],
+    required: true
+  },
+
+  tournamentId: {
+    type: Schema.Types.ObjectId,
+    ref: "Tournament"
+  },
+
+  paymentId: {
+    type: Schema.Types.ObjectId,
+    ref: "Payment"
+  },
+
+  withdrawalId: {
+    type: Schema.Types.ObjectId,
+    ref: "Withdrawal"
+  }
+
+}, { timestamps: true });
+
+export const Transaction=mongoose.model("Transaction",TransactionSchema)
---
+++ Backend/src/modules/withdrawl/withdrawl.model.ts
@@ -0,0 +1,27 @@
+import mongoose, { Document } from "mongoose";
+
+export interface IWithdrawl extends Document{
+    userId:mongoose.Schema.Types.ObjectId,
+    amount:Number,
+    status:"pending"|"success"|"rejected"
+}
+
+
+const WithdrawlSchema=new mongoose.Schema<IWithdrawl> ({
+        userId:{
+            type:mongoose.Types.ObjectId,
+            ref:"User",
+            required:true
+        },
+        amount:{
+            type:Number,
+            required:true
+        },
+        status:{
+            type:String,
+            enum:["pending", "success", "rejected"],
+            default:"pending"
+        }
+},{ timestamps:true})
+
+export const Withdrawl=mongoose.model("Withdrawl",WithdrawlSchema)  
